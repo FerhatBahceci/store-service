d58469d7935947028091bdb68db151bd1300f3c8
ferhatb <ferhat.bahceci.91@gmail.com>
2020-12-05 01:07:24 +0100

    Added coroutineContextFactory

--- src/main/kotlin/store/service/StoreService.kt
+++ src/main/kotlin/store/service/StoreService.kt
@@ -1,8 +1,10 @@
-/*
 package store.service
 
 import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Context
+import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcService
+import io.micronaut.scheduling.TaskExecutors
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.newFixedThreadPoolContext
@@ -13,13 +15,10 @@ import proto.store.service.*
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-@Bean
-fun coroutineContext() = newFixedThreadPoolContext(4, "grpc-server")
-
 @ExperimentalSerializationApi
 @GrpcService
-class StoreServiceImpl @ObsoleteCoroutinesApi constructor(private val gateway: StoreGateway,
-                                                          @Inject override val coroutineContext: CoroutineContext = newFixedThreadPoolContext(4, "grpc-server")) :
+class StoreServiceImpl @ObsoleteCoroutinesApi constructor(@Inject private val gateway: StoreGateway,
+                                                          @Inject override val coroutineContext: CoroutineContext) :
         StoreServiceGrpcKt.StoreServiceCoroutineImplBase(), CoroutineScope {
 
     override suspend fun getStoreByType(request: GetStoreByTypeRequest): GetStoresResponse =
@@ -39,10 +38,17 @@ class StoreServiceImpl @ObsoleteCoroutinesApi constructor(private val gateway: S
     override suspend fun deleteStore(request: DeleteStoreByIdRequest): DeleteStoreResponse =
             gateway.deleteStore(request.id.toString()).let { DeleteStoreResponse.newBuilder().build() }
 
+    //TODO remove mappers and use kotlin-proto-serialization if possible
     private fun List<Store>.mapToProto() = Stores.newBuilder().addAllStores(this.map { it.mapToProto() })
 
     private fun proto.store.service.Store.mapFromProto() = decodeFromByteArray(Store.serializer(), this.toByteArray())
 
     private fun Store.mapToProto() = proto.store.service.Store.parseFrom(encodeToByteArray(Store.serializer(), this))
 }
-*/
+
+@Factory
+class CoroutineContextFactory {
+
+    @Bean
+    fun coroutineContext() = newFixedThreadPoolContext(4, "grpc-thread-pool")
+}
