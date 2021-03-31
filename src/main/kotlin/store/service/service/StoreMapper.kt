4e59dfaa470fbb3198fdedbd222556ff5e987db8
ferhatb <ferhat.bahceci.91@gmail.com>
2021-03-31 23:16:00 +0200

    Adding tests

--- src/main/kotlin/store/service/service/StoreMapper.kt
+++ src/main/kotlin/store/service/service/StoreMapper.kt
@@ -13,34 +13,34 @@ class StoreMapper {
     companion object {
 
         fun store.service.gateway.Store.mapToProtoStore() =
-            Store.newBuilder()
-                .setCoordinates(coordinates?.mapToProtoCoordinates())
-                .setDescription(description)
-                .setId(id)
-                .setName(name)
-                .setPhoneNo(phoneNo)
-                .putAllHours(hours?.mapToProtoHours())
-                .setType(type?.mapToProtoStoreType())
-                .build()
+                Store.newBuilder()
+                        .setCoordinates(coordinates?.mapToProtoCoordinates())
+                        .setDescription(description)
+                        .setId(id)
+                        .setName(name)
+                        .setPhoneNo(phoneNo)
+                        .putAllHours(hours?.mapToProtoHours())
+                        .setType(type?.mapToProtoStoreType())
+                        .build()
 
         private fun store.service.gateway.Store.Coordinates.mapToProtoCoordinates() =
-            Coordinates.newBuilder()
-                .setLatitude(latitude ?: 0)
-                .setLongitude(longitude ?: 0)
-                .build()
+                Coordinates.newBuilder()
+                        .setLatitude(latitude ?: 0)
+                        .setLongitude(longitude ?: 0)
+                        .build()
 
         private fun store.service.gateway.Store.Type.mapToProtoStoreType() =
-            Type.forNumber(this.ordinal)
+                Type.forNumber(this.ordinal)
 
         private fun Map<String, store.service.gateway.Store.OpeningHours>.mapToProtoHours() =
-            map { it.key to it.value.mapToProtoOpeningHours() }.toMap()
+                map { it.key to it.value.mapToProtoOpeningHours() }.toMap()
 
         private fun store.service.gateway.Store.OpeningHours.mapToProtoOpeningHours() =
-            OpeningHours.newBuilder()
-                .setOpening(
-                    Timestamp.newBuilder().setSeconds(this.opening?.seconds ?: 0).setNanos(this.opening?.nanos ?: 0)
-                ).setClosing(
-                    Timestamp.newBuilder().setSeconds(this.closing?.seconds ?: 0).setNanos(this.closing?.nanos ?: 0)
-                ).build()
+                OpeningHours.newBuilder()
+                        .setOpening(Timestamp.newBuilder().setSeconds(this.opening?.seconds
+                                ?: 0).setNanos(this.opening?.nanos ?: 0)
+                        ).setClosing(Timestamp.newBuilder().setSeconds(this.closing?.seconds
+                                ?: 0).setNanos(this.closing?.nanos ?: 0)
+                        ).build()
     }
 }
--- src/test/kotlin/store/service/StoreServiceImplTest.kt
+++ src/test/kotlin/store/service/StoreServiceImplTest.kt
@@ -1,17 +1,22 @@
 package store.service
 
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.BehaviorSpec
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import kotlinx.serialization.ExperimentalSerializationApi
-import proto.store.service.CreateStoreRequest
-import io.kotlintest.AbstractProjectConfig
-import io.micronaut.runtime.context.env.ConfigurationAdvice
-import io.micronaut.test.extensions.kotlintest.MicronautKotlinTestExtension
+import io.kotlintest.specs.ShouldSpec
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import proto.store.service.*
+import proto.store.service.CreateStoreRequest
+import proto.store.service.DeleteStoreByIdRequest
+import proto.store.service.GetAllStoresRequest
+import proto.store.service.GetStoreByNameRequest
+import proto.store.service.UpdateStoreRequest
+import store.service.gateway.Store
 import store.service.gateway.StoreGateway
 import store.service.service.StoreMapper.Companion.mapToProtoStore
 import store.service.service.StoreServiceImpl
@@ -19,7 +24,7 @@ import store.service.service.StoreServiceImpl
 @ObsoleteCoroutinesApi
 @ExperimentalSerializationApi
 @MicronautTest
-class StoreServiceImplTest : BehaviorSpec({
+class StoreServiceImplTest : ShouldSpec({
 
     val store = DummyData.createStore("IKEA")
 
@@ -27,25 +32,66 @@ class StoreServiceImplTest : BehaviorSpec({
         coEvery {
             createStore(CreateStoreRequest(store))
         } returns Unit
+        coEvery {
+            getStoreByName(GetStoreByNameRequest(store.name))
+        } returns store
+        coEvery {
+            getAllStores(GetAllStoresRequest())
+        } returns listOf(store, store)
+        coEvery {
+            getStoreByType(GetStoreByTypeRequest(store.type))
+        } returns listOf(store)
+        coEvery {
+            deleteStore(DeleteStoreByIdRequest(store.id))
+        } returns Unit
+        coEvery {
+            updateStore(UpdateStoreRequest(store.id, store))
+        } returns Unit
     }
 
-    val storeService =  StoreServiceImpl(gateway = storeGateway, coroutineContext = newSingleThreadContext("gprc-test"))
+    val storeService = StoreServiceImpl(gateway = storeGateway, coroutineContext = newSingleThreadContext("gprc-test"))
 
-    given("the store service") {
+    should("create a store") {
+        val createRequest = CreateStoreRequest.newBuilder().setStore(store.mapToProtoStore()).build()
+        val createResponse = storeService.createStore(createRequest)
+        createResponse.response.status shouldBe 201
+    }
 
-        `when`("a store is created with the service") {
-            val createRequest = CreateStoreRequest.newBuilder().setStore(store.mapToProtoStore()).build()
-            val createResponse = storeService.createStore(createRequest)
-            then("the response should be 201") {
-                createResponse.response.status shouldBe 201
-            }
-        }
+    should("fetch a store by name") {
+        val getStoreByNameRequest = GetStoreByNameRequest.newBuilder()
+                .setName(store.name)
+                .build()
+        val getStoreByNameResponse = storeService.getStoreByName(getStoreByNameRequest)
+        ProtoBuf.decodeFromByteArray<Store>(getStoreByNameResponse.store.toByteArray()) shouldBe store
+    }
 
-        /* `when`("the service is called with 3") {
-             val result = mathService.compute(3)
-             then("the result is 12") {
-                 result shouldBe 12
-             }
-         }*/
+    should("fetch all stores") {
+        val getAllStoresRequest = GetAllStoresRequest.getDefaultInstance()
+        val getAllStoresResponse = storeService.getAllStores(getAllStoresRequest)
+        getAllStoresResponse.stores.storesCount shouldBe 2
+        getAllStoresResponse.stores.storesList.map { ProtoBuf.decodeFromByteArray<Store>(it.toByteArray()) } shouldBe listOf(store, store)
+    }
+
+    should("fetch all stores by type") {
+        val getStoreByTypeRequest = proto.store.service.GetStoreByTypeRequest.newBuilder()
+                .setType(Type.valueOf(store.type?.name ?: Store.Type.UNKNOWN.name))
+                .build()
+        val getAllStoresResponse = storeService.getStoreByType(getStoreByTypeRequest)
+        getAllStoresResponse.stores.storesCount shouldBe 1
+        getAllStoresResponse.stores.storesList.map { ProtoBuf.decodeFromByteArray<Store>(it.toByteArray()) } shouldBe listOf(store)
+    }
+
+    should("be able to delete a store by id") {
+        val deleteStoreByIdRequest = DeleteStoreByIdRequest.newBuilder()
+                .setId(store.id)
+                .build()
+        val getStoreByNameResponse = storeService.deleteStore(deleteStoreByIdRequest)
+        getStoreByNameResponse.response.status shouldBe 204
+    }
+
+    should("be able to update a store by id") {
+        val updateStoreByIdRequest = UpdateStoreRequest.newBuilder().setUpdate(store.mapToProtoStore()).setId(store.id).build()
+        val getStoreByIdResponse = storeService.updateStore(updateStoreByIdRequest)
+        getStoreByIdResponse.response.status shouldBe 204
     }
 })
